4cee039bb5c31092236145f29152c0f328e729f8
Krishna Sangeeth <kskrishnasangeeth@gmail.com>
2018-08-08 02:24:58 +0530

    Fixing container issues and some data wrangling issues. Basic functionality working

--- container/build_and_push.sh
+++ container/build_and_push.sh
@@ -13,8 +13,8 @@ then
     exit 1
 fi
 
-chmod +x fasttext/train.py
-chmod +x fasttext/serve.py
+chmod +x fasttext/train
+chmod +x fasttext/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
